8cee1156117f1c4081f30d84d62dda9e6359f38f
Cameron A Zimmerman <cameronazimmerman@gmail.com>
2021-03-23 13:24:53 -0700

    map scaffold

--- map/map.test.js
+++ map/map.test.js
@@ -1,3 +1,5 @@
+const map = require('./map.js');
+
 describe('map function', () => {
   it('takes an array, and a callback and creates a new array with the return values of the callback for each element in the original array', () => {
     const mapped = map([1, 2, 3], (n) => n + 1);
