1836fe6b3c31ff7ad2bf8e1cdf4dd2dc968197f9
Cameron A Zimmerman <cameronazimmerman@gmail.com>
2021-03-23 14:02:21 -0700

    create initial test for reduce

---
+++ reduce/reduce.test.js
@@ -0,0 +1,9 @@
+const reduce = require('./reduce.js');
+
+describe('reduce function', () => {
+  it('should take in an array and a callback with parameters item and accumulator and optionally an initial value parameter. for each element of the array invoke the callback and pass the return value into the next function call', () => {
+    const sum = reduce([1, 2, 3, 4], (accumulator, item) => accumulator + item);
+
+    expect(sum).toEqual(10);
+  })
+})
