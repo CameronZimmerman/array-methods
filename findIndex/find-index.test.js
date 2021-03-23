5a75cd59599b0cafe4d4d6b53160ee2153e6e139
Cameron A Zimmerman <cameronazimmerman@gmail.com>
2021-03-23 13:46:31 -0700

    create test for findIndex function

---
+++ findIndex/find-index.test.js
@@ -0,0 +1,9 @@
+const findIndex = require('./find-index.js');
+
+describe('findIndex function', () => {
+  it('takes in an array and a callback, then returns the index of the first element in the array which when fed into the callback returns true', () => {
+    const index = findIndex([1, 2, '3', 4], item => typeof item === 'string')
+  });
+  
+  expect(index).toEqual(2);
+})
