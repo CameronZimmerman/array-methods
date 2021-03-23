20d92bea8fb3805ac475b08201707143cff13df9
Cameron A Zimmerman <cameronazimmerman@gmail.com>
2021-03-23 13:29:26 -0700

    make map function

--- map/map.js
+++ map/map.js
@@ -1,5 +1,11 @@
-const map = () => {
-
+const map = (array, callback) => {
+  let newArray = array;
+  for (let i = 0; i < array.length; i++) {
+    const element = array[i];
+    let newElement = callback(element);
+    newArray[i] = newElement;
+  }
+  return newArray;
 }
 
 module.exports = map;
