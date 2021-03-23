04e0ee186150e6ebdd7f6a3ca1ffe14ea8762a17
Cameron A Zimmerman <cameronazimmerman@gmail.com>
2021-03-23 14:19:06 -0700

    reduce function

--- reduce/reduce.js
+++ reduce/reduce.js
@@ -1,5 +1,11 @@
-const reduce = () => {
+const reduce = (array, callback, initialValue) => {
+  accumulator = typeof initialValue === 'undefined' ? array[0] : initialValue;
 
-}
+  for (let i = 1; i < array.length; i++) {
+    const element = array[i];
+    accumulator = callback(accumulator, element);
+  }
+  return accumulator;
+};
 
 module.exports = reduce;
